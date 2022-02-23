0e65ed5d1bb22f07095db4c75b91f7323923e916
David Silveira <dsilveira@Davids-MacBook-Pro.local>
2022-02-23 14:57:15 -0500

    registered Review model to be viewabe on the django admin site

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from books.models import Book, Author
+from books.models import Book, Review, Author
 
 admin.site.register(Book)
-# admin.site.register(Review)
+admin.site.register(Review)
 admin.site.register(Author)

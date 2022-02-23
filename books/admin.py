5b734e41ca96c6c8e3ccece766453c7115c3375e
David Silveira <dsilveira@Davids-MacBook-Pro.local>
2022-02-23 10:09:07 -0500

    added an authors class and defined it as a many to many relationship then defined the context in views.py to query the right data in the database

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from books.models import Book, Review
+from books.models import Book, Author
 
 admin.site.register(Book)
-admin.site.register(Review)
+# admin.site.register(Review)
+admin.site.register(Author)
--- books/models.py
+++ books/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+class Author(models.Model):
+    name = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=256)
     pageCount = models.IntegerField(default=0)
     thumbnailUrl = models.CharField(max_length=256, null=True)
     shortDescription = models.CharField(max_length=256, null=True)
     longDescription = models.TextField(null=True)
+    authors = models.ManyToManyField(Author)
 
     def __str__(self):
         return f"{self.id} {self.title}"
@@ -15,3 +24,6 @@ class Review(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
+
+
+

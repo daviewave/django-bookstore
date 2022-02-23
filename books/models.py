f60f6758bd75ccecad7cccf2c8174615edd1f073
David Silveira <dsilveira@Davids-MacBook-Pro.local>
2022-02-22 21:46:10 -0500

    added classes to the file that will represent the db schema

--- books/models.py
+++ books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    pageCount = models.IntegerField(default=0)
+    thumbnailUrl = models.CharField(max_length=256, null=True)
+    shortDescription = models.CharField(max_length=256, null=True)
+    longDescription = models.TextField(null=True)
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
+
+
+class Review(models.Model):
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)

a83613d80a83fd0bcc2f2f60a9a64753a71cc781
David Silveira <dsilveira@Davids-MacBook-Pro.local>
2022-02-23 15:03:34 -0500

    changed the display of reviews on admin site

--- books/models.py
+++ books/models.py
@@ -28,7 +28,7 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return f"{self.user}"
+        return f"{self.body}"
 
 
 

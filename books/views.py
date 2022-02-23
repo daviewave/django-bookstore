f709a3ae0413947334d91edb622abc6e87155f66
David Silveira <dsilveira@Davids-MacBook-Pro.local>
2022-02-22 21:49:18 -0500

    added methods/queries for classes specified in models.py

--- books/views.py
+++ books/views.py
@@ -1,17 +1,41 @@
-from django.shortcuts import render
-import json
+from multiprocessing import context
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView
 
-books_data = open('/Users/dsilveira/Desktop/python-venv-4.2.0/Bookstore-Project/books.json').read()
-data = json.loads(books_data)
+from books.models import Book, Review
 
-def index(request):
-    context = {'books':data}
-    return render(request, 'books/index.html', context)
+# GENERIC VIEWS #
+class BookListView(ListView):
+    # template_name = 'books/index.html'
+    # context_object_name = 'books'
 
-def show(request, id):
-    singleBook = list()
-    for book  in data:
-        if book['id'] == id:
-            singleBook = book
-    context = {'book':singleBook}
-    return render(request, 'books/show.html', context)
+    def get_queryset(self):
+        return Book.objects.all()
+
+
+class BookDetailView(DetailView):
+    model = Book
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = context['book'].review_set.order_by('-created_at')
+        return context
+
+# VIEWS #
+# def index(request):
+#     dbData = Book.objects.all()
+#     context = {'books':dbData}
+#     return render(request, 'books/index.html', context)
+
+# def show(request, id):
+#     singleBook = get_object_or_404(Book, pk=id)
+#     reviews = Review.objects.filter(book_id=id).order_by('-id')
+#     context = {'book':singleBook, 'reviews':reviews}
+#     return render(request, 'books/show.html', context)
+
+def review(request, id):
+    body = request.POST['review']
+    new_review = Review(body=body, book_id=id)
+    new_review.save()
+    return redirect('/book')
+   
